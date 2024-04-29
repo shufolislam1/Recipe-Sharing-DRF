8526d8377b5b13a69e6e391be2fa245b4a9587c8
shufolislam1 <shufolislam1255@gmail.com>
2024-04-30 02:11:28 +0600

    deploy remaining

--- core/serializers.py
+++ core/serializers.py
@@ -26,3 +26,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
         account.is_active = False
         account.save()
         return account
+    
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required = True)
+    password = serializers.CharField(required = True)
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('register/', views.UserRegistrationApiView.as_view(), name='register')
+    path('register/', views.UserRegistrationApiView.as_view(), name='register'),
+    path('login/', views.UserLoginApiView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('active/<uid64>/<token>', views.activate, name='activate')
 ]
