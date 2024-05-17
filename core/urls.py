0a7d8565fa8d846b5277a49914b38c25a200ad28
shufolislam1 <shufolislam1255@gmail.com>
2024-05-17 10:48:58 +0600

    test done

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('register/', views.UserRegistrationApiView.as_view(), name='register'),
     path('login/', views.UserLoginApiView.as_view(), name='login'),
-    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('logout/', views.UserLogoutAPIView.as_view(), name='logout'),
     path('active/<uid64>/<token>', views.activate, name='activate')
 ]
